c3a64de96888327358129bbd8876fc7f016c510f
Ryan Osborn <ryan@resrvoir.com>
2020-02-15 22:22:25 -0500

    Sync to web dir

--- pre_deploy_local.sh
+++ pre_deploy_local.sh
@@ -1,5 +1,5 @@
 environment=${1}
 project=${2}
 echo "rsyncing .env_${environment}_${project}"
-rsync .env_${environment}_${project} ec2-user@rmc2:/var/www/html/${project}_api_${environment}/current
+rsync .env_${environment}_${project} ec2-user@rmc2:/var/www/html/${project}_web_${environment}/current
 echo "rsyncing done"
